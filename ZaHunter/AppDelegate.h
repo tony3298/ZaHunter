893d9ecb9d42b907aa4f7786dc9446ffe83e5a42
tony3298 <tonydakhoul@yahoo.com>
2015-05-28 12:57:26 -0500

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Tony Dakhoul on 5/28/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZaHunter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZaHunter
+//
+//  Created by Tony Dakhoul on 5/28/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
